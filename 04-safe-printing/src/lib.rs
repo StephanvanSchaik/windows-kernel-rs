247abc095ab490148ba4c6c173dab4b273286a9e
S.J.R. van Schaik <stephan@synkhronix.com>
2021-09-25 01:21:54 -0400

    04-safe-printing: update code

--- 04-safe-printing/src/lib.rs
+++ 04-safe-printing/src/lib.rs
@@ -1,36 +1,19 @@
 #![no_std]
 
-use core::panic::PanicInfo;
-use windows_kernel_rs::println;
-use windows_kernel_rs::alloc::KernelAlloc;
-use windows_kernel_sys::base::{DRIVER_OBJECT, NTSTATUS, STATUS_SUCCESS, UNICODE_STRING};
+use windows_kernel_rs::{Driver, Error, kernel_module, KernelModule, println};
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+struct Module;
+
+impl KernelModule for Module {
+    fn init(_: Driver, _: &str) -> Result<Self, Error> {
+        println!("Hello, world!");
+
+        Ok(Module)
+    }
+
+    fn cleanup(&mut self, _: Driver) {
+        println!("Bye bye!");
+    }
 }
 
-#[used]
-#[no_mangle]
-pub static _fltused: i32 = 0;
-
-#[no_mangle]
-pub extern "system" fn __CxxFrameHandler3(_: *mut u8, _: *mut u8, _: *mut u8, _: *mut u8) -> i32 {
-    unimplemented!()
-}
-
-#[global_allocator]
-static GLOBAL: KernelAlloc = KernelAlloc;
-
-#[no_mangle]
-pub extern "system" fn driver_entry(driver: &mut DRIVER_OBJECT, _: &UNICODE_STRING) -> NTSTATUS {
-    println!("Hello, world!");
-
-    driver.DriverUnload = Some(driver_exit);
-
-    STATUS_SUCCESS
-}
-
-pub unsafe extern "C" fn driver_exit(_driver: *mut DRIVER_OBJECT) {
-    println!("Bye bye!");
-}
+kernel_module!(Module);
