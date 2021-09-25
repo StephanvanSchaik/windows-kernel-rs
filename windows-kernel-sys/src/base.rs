6c7189ad49b7cea22dd34eb4361c14c073d96667
S.J.R. van Schaik <stephan@synkhronix.com>
2021-09-25 01:15:02 -0400

    windows-kernel-sys: implement more status codes

--- windows-kernel-sys/src/base.rs
+++ windows-kernel-sys/src/base.rs
@@ -7,3 +7,7 @@ pub use cty::*;
 include!(concat!(env!("OUT_DIR"), "/base.rs"));
 
 pub const STATUS_SUCCESS: NTSTATUS = 0x00000000;
+pub const STATUS_UNSUCCESSFUL: NTSTATUS = 0xC0000001 as u32 as i32;
+pub const STATUS_NOT_IMPLEMENTED: NTSTATUS = 0xC0000002 as u32 as i32;
+pub const STATUS_ACCESS_VIOLATION: NTSTATUS = 0xC0000005 as u32 as i32;
+pub const STATUS_INVALID_PARAMETER: NTSTATUS = 0xC000000D as u32 as i32;
