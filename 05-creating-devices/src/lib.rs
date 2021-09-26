474342bad8a590692a6544131f965f824b64325b
S.J.R. van Schaik <stephan@synkhronix.com>
2021-09-26 08:52:37 -0400

    05-creating-devices: add example on creating devices

---
+++ 05-creating-devices/src/lib.rs
@@ -0,0 +1,55 @@
+#![no_std]
+
+use windows_kernel_rs::{Access, Device, DeviceFlags, DeviceOperations, DeviceType, Driver, Error, kernel_module, KernelModule, println, SymbolicLink, UserPtr};
+
+struct MyDevice;
+
+impl DeviceOperations for MyDevice {
+    fn create(&mut self, device: &Device) -> Result<(), Error> {
+        println!("userspace opened the device");
+        Ok(())
+    }
+
+    fn close(&mut self, device: &Device) -> Result<(), Error> {
+        println!("userspace closed the device");
+        Ok(())
+    }
+
+    fn cleanup(&mut self, device: &Device) -> Result<(), Error> {
+        println!("device is no longer in use by userspace");
+        Ok(())
+    }
+
+    fn ioctl(&mut self, device: &Device, ioctl_num: u32, user_ptr: &mut UserPtr) -> Result<(), Error> {
+        println!("request from userspace");
+        Ok(())
+    }
+}
+
+struct Module {
+    _device: Device,
+    _symbolic_link: SymbolicLink,
+}
+
+impl KernelModule for Module {
+    fn init(mut driver: Driver, _: &str) -> Result<Self, Error> {
+        let device = driver.create_device(
+            "\\Device\\Example",
+            DeviceType::Unknown,
+            DeviceFlags::SECURE_OPEN,
+            Access::NonExclusive,
+            MyDevice,
+        )?;
+        let symbolic_link = SymbolicLink::new("\\??\\Example", "\\Device\\Example")?;
+
+        Ok(Module {
+            _device: device,
+            _symbolic_link: symbolic_link,
+        })
+    }
+
+    fn cleanup(&mut self, _driver: Driver) {
+    }
+}
+
+kernel_module!(Module);
