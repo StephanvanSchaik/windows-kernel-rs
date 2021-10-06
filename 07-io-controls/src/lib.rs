f146b8dc636aeb688996792393adcc75b1927c55
S.J.R. van Schaik <stephan@synkhronix.com>
2021-10-06 00:24:31 -0400

    07-io-controls: implement example IO controls

--- 07-io-controls/src/lib.rs
+++ 07-io-controls/src/lib.rs
@@ -1,14 +1,37 @@
 #![no_std]
 
-use windows_kernel_rs::{Access, ControlCode, Device, DeviceFlags, DeviceOperations, DeviceType, Driver, Error, IoRequest, kernel_module, KernelModule, println, SymbolicLink};
+use windows_kernel_rs::{Access, ControlCode, Device, DeviceFlags, DeviceOperations, DeviceType, Driver, Error, IoControlRequest, kernel_module, KernelModule, println, RequiredAccess, SymbolicLink};
 
-struct MyDevice;
+struct MyDevice {
+    value: u32,
+}
+
+const IOCTL_PRINT_VALUE: u32 = 0x800;
+const IOCTL_READ_VALUE:  u32 = 0x801;
+const IOCTL_WRITE_VALUE: u32 = 0x802;
 
 impl DeviceOperations for MyDevice {
-    fn ioctl(&mut self, _device: &Device, request: &IoRequest) -> Result<(), Error> {
-        println!("{:?}", request.control_code().unwrap());
+    fn ioctl(&mut self, _device: &Device, request: &IoControlRequest) -> Result<(), Error> {
+        match request.control_code() {
+            ControlCode(DeviceType::Unknown, _, IOCTL_PRINT_VALUE, _) => {
+                println!("value: {}", self.value);
 
-        request.complete(Ok(0));
+                request.complete(Ok(0));
+            }
+            ControlCode(DeviceType::Unknown, RequiredAccess::READ_DATA, IOCTL_READ_VALUE, _) => {
+                request.user_ptr().write(&self.value)?;
+
+                request.complete(Ok(core::mem::size_of::<u32>() as u32))
+            }
+            ControlCode(DeviceType::Unknown, RequiredAccess::WRITE_DATA, IOCTL_WRITE_VALUE, _) => {
+                request.user_ptr().read(&mut self.value)?;
+
+                request.complete(Ok(0))
+            }
+            _ => {
+                Err(Error::INVALID_PARAMETER)?
+            }
+        }
 
         Ok(())
     }
@@ -26,7 +49,9 @@ impl KernelModule for Module {
             DeviceType::Unknown,
             DeviceFlags::SECURE_OPEN,
             Access::NonExclusive,
-            MyDevice,
+            MyDevice {
+                value: 0,
+            },
         )?;
         let symbolic_link = SymbolicLink::new("\\??\\Example", "\\Device\\Example")?;
 
