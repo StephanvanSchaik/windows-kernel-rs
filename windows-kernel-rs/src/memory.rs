68b83fb25cedf894b168c2fc147605dfb0865a3b
S.J.R. van Schaik <stephan@synkhronix.com>
2021-10-12 13:36:27 -0400

    windows-kernel-rs: introduce system feature, clean up address types and implement IoMapping

--- windows-kernel-rs/src/memory.rs
+++ windows-kernel-rs/src/memory.rs
@@ -1,19 +1,119 @@
 use crate::error::{Error, IntoResult};
 use windows_kernel_sys::base::{MM_COPY_ADDRESS, MM_COPY_MEMORY_PHYSICAL, MM_COPY_MEMORY_VIRTUAL, PHYSICAL_ADDRESS};
-use windows_kernel_sys::ntoskrnl::{MmCopyMemory, MmGetVirtualForPhysical};
+use windows_kernel_sys::base::_MEMORY_CACHING_TYPE as MEMORY_CACHING_TYPE;
+
+#[repr(i32)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum MemoryCaching {
+    NonCached              = MEMORY_CACHING_TYPE::MmNonCached,
+    Cached                 = MEMORY_CACHING_TYPE::MmCached,
+    WriteCombined          = MEMORY_CACHING_TYPE::MmWriteCombined,
+    #[cfg(feature = "system")]
+    HardwareCoherentCached = MEMORY_CACHING_TYPE::MmHardwareCoherentCached,
+    #[cfg(feature = "system")]
+    NonCachedUnordered     = MEMORY_CACHING_TYPE::MmNonCachedUnordered,
+    #[cfg(feature = "system")]
+    USWCCached             = MEMORY_CACHING_TYPE::MmUSWCCached,
+    NotMapped              = MEMORY_CACHING_TYPE::MmNotMapped,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct PhysicalAddress(u64);
+
+impl From<u64> for PhysicalAddress {
+    fn from(value: u64) -> Self {
+        Self(value)
+    }
+}
+
+impl Into<u64> for PhysicalAddress {
+    fn into(self) -> u64 {
+        self.0
+    }
+}
+
+impl From<PHYSICAL_ADDRESS> for PhysicalAddress {
+    fn from(value: PHYSICAL_ADDRESS) -> Self {
+        Self(unsafe { value.QuadPart } as _)
+    }
+}
+
+impl Into<PHYSICAL_ADDRESS> for PhysicalAddress {
+    fn into(self) -> PHYSICAL_ADDRESS {
+        let mut addr: PHYSICAL_ADDRESS = unsafe { core::mem::zeroed() };
+
+        addr.QuadPart = self.0 as _;
+
+        addr
+    }
+}
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum CopyAddress {
-    Virtual(u64),
-    Physical(u64),
+    Virtual(*mut core::ffi::c_void),
+    Physical(PhysicalAddress),
 }
 
-pub fn get_virtual_for_physical(addr: u64) -> u64 {
-    let mut phys_addr: PHYSICAL_ADDRESS = unsafe { core::mem::zeroed() };
+impl Into<(u32, MM_COPY_ADDRESS)> for CopyAddress {
+    fn into(self) -> (u32, MM_COPY_ADDRESS) {
+        let mut copy_addr: MM_COPY_ADDRESS = unsafe {
+            core::mem::zeroed()
+        };
 
-    phys_addr.QuadPart = addr as _;
+        let flags = match self {
+            CopyAddress::Virtual(addr) => {
+                copy_addr.__bindgen_anon_1.VirtualAddress = addr as _;
+                MM_COPY_MEMORY_VIRTUAL
+            }
+            CopyAddress::Physical(addr) => {
+                copy_addr.__bindgen_anon_1.PhysicalAddress = addr.into();
+                MM_COPY_MEMORY_PHYSICAL
+            }
+        };
 
-    let virt_addr = unsafe { MmGetVirtualForPhysical(phys_addr) };
+        (flags, copy_addr)
+    }
+}
+
+pub struct IoMapping {
+    ptr: *mut core::ffi::c_void,
+    size: usize,
+}
+
+impl IoMapping {
+    pub fn new(addr: PhysicalAddress, size: usize, caching: MemoryCaching) -> Result<Self, Error> {
+        use windows_kernel_sys::ntoskrnl::MmMapIoSpace;
+
+        let ptr = unsafe {
+            MmMapIoSpace(addr.into(), size as _, caching as _)
+        };
+
+        if ptr.is_null() {
+            return Err(Error::INVALID_PARAMETER);
+        }
+
+        Ok(Self {
+            ptr,
+            size,
+        })
+    }
+}
+
+impl Drop for IoMapping {
+    fn drop(&mut self) {
+        use windows_kernel_sys::ntoskrnl::MmUnmapIoSpace;
+
+        unsafe {
+            MmUnmapIoSpace(self.ptr, self.size as _);
+        }
+    }
+}
+
+#[cfg(feature = "system")]
+pub fn get_virtual_for_physical(addr: PhysicalAddress) -> *mut core::ffi::c_void {
+    use windows_kernel_sys::ntoskrnl::MmGetVirtualForPhysical;
+
+    let virt_addr = unsafe { MmGetVirtualForPhysical(addr.into()) };
 
     virt_addr as _
 }
@@ -22,19 +122,10 @@ pub fn read_memory(
     buffer: &mut [u8],
     source: CopyAddress,
 ) -> Result<usize, Error> {
-    let mut copy_addr: MM_COPY_ADDRESS = unsafe { core::mem::zeroed() };
-    let mut bytes = 0;
+    use windows_kernel_sys::ntoskrnl::MmCopyMemory;
 
-    let flags = match source {
-        CopyAddress::Virtual(addr) => {
-            copy_addr.__bindgen_anon_1.VirtualAddress = addr as _;
-            MM_COPY_MEMORY_VIRTUAL
-        }
-        CopyAddress::Physical(addr) => {
-            copy_addr.__bindgen_anon_1.PhysicalAddress.QuadPart = addr as _;
-            MM_COPY_MEMORY_PHYSICAL
-        }
-    };
+    let (flags, copy_addr) = source.into();
+    let mut bytes = 0;
 
     unsafe {
         MmCopyMemory(
@@ -49,10 +140,13 @@ pub fn read_memory(
     Ok(bytes as _)
 }
 
+#[cfg(feature = "system")]
 pub fn write_memory(
     target: CopyAddress,
     buffer: &[u8],
 ) -> Result<usize, Error> {
+    use windows_kernel_sys::ntoskrnl::MmCopyMemory;
+
     let mut copy_addr: MM_COPY_ADDRESS = unsafe { core::mem::zeroed() };
     let mut bytes = 0;
 
