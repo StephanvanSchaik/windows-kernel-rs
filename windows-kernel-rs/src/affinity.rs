54aedd783619c041875f4d1300401f55da62f441
S.J.R. van Schaik <stephan@synkhronix.com>
2021-10-16 21:27:02 -0400

    windows-kernel-rs: document affinity.rs

--- windows-kernel-rs/src/affinity.rs
+++ windows-kernel-rs/src/affinity.rs
@@ -1,3 +1,6 @@
+//! This module provides functions to get information about the logical CPUs in the system, and to
+//! run closures on specific or all CPUs.
+
 use crate::error::{Error, IntoResult};
 use windows_kernel_sys::base::{ALL_PROCESSOR_GROUPS, GROUP_AFFINITY, PROCESSOR_NUMBER, ULONG_PTR};
 use windows_kernel_sys::ntoskrnl::{
@@ -6,18 +9,24 @@ use windows_kernel_sys::ntoskrnl::{
     KeSetSystemGroupAffinityThread,
 };
 
+/// Uses [`KeGetCurrentProcessorNumberEx`] to get the logical number associated with the CPU that
+/// is currently running our code.
 pub fn get_current_cpu_num() -> u32 {
     unsafe {
         KeGetCurrentProcessorNumberEx(core::ptr::null_mut())
     }
 }
 
+/// Uses [`KeQueryActiveProcessorCountEx`] to get the number of CPUs in the system, that is all the
+/// CPUs from all the different CPU groups are counted, such that each of them has a logical
+/// number.
 pub fn get_cpu_count() -> u32 {
     unsafe {
         KeQueryActiveProcessorCountEx(ALL_PROCESSOR_GROUPS as _)
     }
 }
 
+/// This is the callback used by [`run_on_each_cpu_parallel`] to run the closure on all CPUs.
 unsafe extern "C" fn broadcast_callback<F>(
     context: ULONG_PTR,
 ) -> ULONG_PTR
@@ -30,6 +39,12 @@ where
     0
 }
 
+/// Runs the given closure on all CPUs in the system without interrupting all CPUs to force them to
+/// switch to kernel mode. Instead, this is a more graceful version that simply relies on
+/// [`run_on_cpu`] to switch to all the possible CPUs by configuring the affinity, and execute the
+/// closure on the selected CPU. Upon executing the closure on all CPUs, the affinity is restored.
+/// Also see [`run_on_each_cpu_parallel`] which is a more aggressive version that relies on an IPI
+/// to run a given closure on all CPUs in parallel.
 pub fn run_on_each_cpu<F>(
     f: &mut F,
 ) -> Result<(), Error>
@@ -42,7 +57,12 @@ where
 
     Ok(())
 }
- 
+
+/// Runs the given closure on all CPUs in the system by broadcasting an Inter-Processor Interrupt
+/// (IPI) to interrupt all CPUs to force them to switch to kernel mode to run the given closure.
+/// Upon execution of the closure, these CPUs resume their work. Also see [`run_on_each_cpu`] which
+/// is a friendlier version that does not rely on an IPI but instead configures the affinity to run
+/// a given a closure on all CPUs.
 pub fn run_on_each_cpu_parallel<F>(
     f: &F,
 )
@@ -54,6 +74,9 @@ where
     }
 }
 
+/// Runs the given closure on the CPU with the given CPU number by temporarily configuring the CPU
+/// affinity to only contain the given CPU number. Upon switching to the selected CPU, the CPU
+/// executes the closure. Then the original affinity is restored.
 pub fn run_on_cpu<F>(
     cpu_num: u32,
     f: &mut F,
