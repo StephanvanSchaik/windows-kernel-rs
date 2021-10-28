04c8d5dcd35648956a5a195505b274f228468ac5
S.J.R. van Schaik <stephan@synkhronix.com>
2021-10-28 01:44:21 -0400

    windows-kernel-rs: document intrinsics

--- windows-kernel-rs/src/intrin.rs
+++ windows-kernel-rs/src/intrin.rs
@@ -1,6 +1,10 @@
 use crate::error::{Error, IntoResult};
 use windows_kernel_sys::intrin::{read_msr_safe, write_msr_safe};
 
+/// Attempts to read the given model-specific register. Accessing an invalid model-specific
+/// register would normally result in a CPU exception. This function uses Structured Exception
+/// Handling (SEH) to safely catch CPU exceptions and to turn them into an [`Error`]. This prevents
+/// a hang.
 pub fn read_msr(register: u32) -> Result<u64, Error> {
     let mut value = 0;
 
@@ -11,6 +15,10 @@ pub fn read_msr(register: u32) -> Result<u64, Error> {
     Ok(value)
 }
 
+/// Attempts to write the given value to the given model-specific register. Accessing an invalid
+/// model-specific register would normally result in a CPU exception. This function uses Structured
+/// Handling (SEH) to safely catch CPU exceptions and to turn them into an [`Error`]. This prevents
+/// a hang.
 pub fn write_msr(register: u32, value: u64) -> Result<(), Error> {
     unsafe {
         write_msr_safe(register, value)
