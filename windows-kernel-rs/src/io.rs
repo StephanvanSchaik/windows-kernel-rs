8a82bc327e3b59d5d98d67bb557dbe3a407be3b3
S.J.R. van Schaik <stephan@synkhronix.com>
2021-10-28 01:39:34 -0400

    windows-kernel-rs: ensure the user string is not passed as the format string

--- windows-kernel-rs/src/io.rs
+++ windows-kernel-rs/src/io.rs
@@ -1,3 +1,4 @@
+use windows_kernel_sys::base::ANSI_STRING;
 use windows_kernel_sys::ntoskrnl::DbgPrint;
 
 #[macro_export]
@@ -13,8 +14,19 @@ macro_rules! println {
 
 #[doc(hidden)]
 pub fn _print(args: core::fmt::Arguments) {
-    let s = alloc::format!("{}\0", args);
+    // Format the string using the `alloc::format!` as this is guaranteed to return a `String`
+    // instead of a `Result` that we would have to `unwrap`. This ensures that this code stays
+    // panic-free.
+    let s = alloc::format!("{}", args);
 
-    // Print the string.
-    unsafe { DbgPrint(s.as_ptr() as _) };
+    // Print the string. We must make sure to not pass this user-supplied string as the format
+    // string, as `DbgPrint` may then format any format specifiers it contains. This could
+    // potentially be an attack vector.
+    let s = ANSI_STRING {
+        Length: s.len() as u16,
+        MaximumLength: s.len() as u16,
+        Buffer: s.as_ptr() as _,
+    };
+
+    unsafe { DbgPrint("%Z\0".as_ptr() as _, &s) };
 }
